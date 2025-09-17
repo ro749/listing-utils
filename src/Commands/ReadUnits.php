368c43128bb5c6b71f074fcf6857ed165239bafa
ro749 <45647699+ro749@users.noreply.github.com>
2025-09-17 13:17:12 -0600

    Update ReadUnits.php

--- src/Commands/ReadUnits.php
+++ src/Commands/ReadUnits.php
@@ -4,7 +4,7 @@ namespace Ro749\ListingUtils\Commands;
 
 use Illuminate\Console\Command;
 use Ro749\SharedUtils\Readers\DbReader;
-
+use Ro749\SharedUtils\Readers\DbUpdate;
 class ReadUnits extends Command
 {
     /**
@@ -12,7 +12,7 @@ class ReadUnits extends Command
      *
      * @var string
      */
-    protected $signature = 'read:cvs {file}';
+    protected $signature = 'read:cvs {file} {--update : Update instead of create}';
 
     /**
      * The console command description.
@@ -27,11 +27,21 @@ class ReadUnits extends Command
     public function handle()
     {
         $file = $this->argument('file');
-        $reader = new DbReader(
-            table: 'units',
-            required_columns: ['unit','price','status'],
-            add_new_columns: true
-        );
+        if ($this->option('update')) {
+            $reader = new DbUpdate(
+                table: 'units',
+                add_new_columns: false,
+                public_id: 'unit'
+            );
+        }
+        else{
+            $reader = new DbReader(
+                table: 'units',
+                required_columns: ['unit','price','status'],
+                add_new_columns: true
+            );
+        }
+        
         $reader->read_cvs($file);
         $this->call('migrate', [
             '--force' => true
