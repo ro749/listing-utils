ebf2cdc2c66804647f02c8cc2d592e9e892c46b0
ro749 <45647699+ro749@users.noreply.github.com>
2025-12-09 16:21:37 -0600

    Update imageMapPro.js

--- resources/dist/js/imageMapPro.js
+++ resources/dist/js/imageMapPro.js
@@ -1,44 +1,56 @@
 (function ($) {
+
+    $.fn.select_unit = function (unit) {
+        var $this = $(this);
+        choosen_unit($this,unit);
+    };
+
+    function choosen_unit($this,unit){
+        var selected_unit = $this.data('selected_unit');
+        var selected_color = $this.data('selected_color');
+        if(selected_unit){
+            $('[data-title="' + selected_unit + '"]').attr('style', '');
+        }
+        selected_unit = unit;
+        $this.data('selected_unit', unit);
+        if("svg" == $('[data-title="' + unit + '"]').prop("tagName")){
+            $('[data-title="' + unit + '"]').attr('style', 'fill: '+selected_color+' !important;');
+        }
+        else{
+            $('[data-title="' + unit + '"]').attr('style', 'background: '+selected_color+' !important;');
+        }
+        $('#unit-info').show();
+        $('html, body').animate({
+          scrollTop: $("#unit-info").offset().top
+        }, 800);
+        setTimeout(function(){
+            const event = new UIEvent('resize', {
+              bubbles: true,
+              cancelable: false,
+              view: window,
+              detail: 0
+            });
+            window.dispatchEvent(event);
+        }, 166);
+    }
+
     $.fn.imageMapPro = function (options = {}) {
-        var selected_unit = null;
-        var selected_color = null;
+        var $this = $(this);
         function get_unit(unit){
-            
             $.ajax({
                 url: 'imagemappro/'+options.id+'/unit',
                 method: 'GET',
                 dataType: 'json',
                 data: {unit: unit},
                 success: function (response) {
-                    if(selected_unit){
-                        $('[data-title="' + selected_unit + '"]').attr('style', '');
-                    }
-                    selected_unit = unit;
-                    if("svg" == $('[data-title="' + unit + '"]').prop("tagName")){
-                        $('[data-title="' + unit + '"]').attr('style', 'fill: '+selected_color+' !important;');
-                    }
-                    else{
-                        $('[data-title="' + unit + '"]').attr('style', 'background: '+selected_color+' !important;');
-                    }
+                    choosen_unit($this,unit);
                     $(document).trigger('selected-unit', [{ unit: response }]);
-                    $('#unit-info').show();
-                    $('html, body').animate({
-                      scrollTop: $("#unit-info").offset().top
-                    }, 800);
-                    setTimeout(function(){
-                        const event = new UIEvent('resize', {
-                          bubbles: true,
-                          cancelable: false,
-                          view: window,
-                          detail: 0
-                        });
-                        window.dispatchEvent(event);
-                    }, 166);
                 }
             });
         } 
 
         ImageMapPro.subscribe((action) =>{
+            var selected_unit = $(this).data('selected_unit');
             if(action.type == "mapInit"){
                 if(ImageMapPro.isMobile()){
                     document.getElementById("image-map-pro").addEventListener("click", function(event) {
@@ -65,13 +77,13 @@
                 $('[data-title="' + selected_unit + '"]').attr('style', 'background: '+selected_color+' !important;');
             }
         });
-
+        var $this = $(this);
         $.ajax({
             url: 'imagemappro/'+options.id+'/map',
             method: 'GET',
             dataType: 'json',
             success: function (response) {
-                selected_color = response.selected_color;
+                $this.data('selected_color', response.selected_color);
                 ImageMapPro.init('#image-map-pro',response.map);
             }
         });
