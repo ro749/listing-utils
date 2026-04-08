706bbf62bdd4151ba9b035779b8b9afbf9e6ea80
ro749 <45647699+ro749@users.noreply.github.com>
2026-04-07 18:35:08 -0600

    Update PlansBase.php

--- src/Plans/PlansBase.php
+++ src/Plans/PlansBase.php
@@ -265,6 +265,7 @@ class PlansBase
     }
     
     public function render($personal_plan = null,$sender = null){
+        Log::info(json_encode($sender,JSON_PRETTY_PRINT));
         return view(config('overrides.views.plans'), [
             'plans' => $this->plans,
             'form'=>$this->form,
