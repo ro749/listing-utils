ce896811f8ab75bb5ea77e7f0c4ed27164e8cbd3
ro749 <45647699+ro749@users.noreply.github.com>
2026-07-01 10:19:42 -0600

    Update sent-whatsapp-safari.blade.php

--- resources/views/Sender/sent-whatsapp-safari.blade.php
+++ resources/views/Sender/sent-whatsapp-safari.blade.php
@@ -4,7 +4,7 @@
         <p id="whatsapp-quotation"></p>
     </div>
     <div class="row" style="justify-content: center; margin:0px;">
-        <a style="display: flex; flex-direction: row; justify-content: center; margin:0px;" href="https://wa.me/{{ $sender->client->phone }}" target="_blank">
+        <a style="display: flex; flex-direction: row; justify-content: center; margin:0px;" href="https://wa.me/52{{ $sender->client->phone }}" target="_blank">
             <button class="btn btn-success btn-round btn-block mb-3" style="width: 40%; margin-top: 6px; margin-left: auto; margin-right: auto;" type="button">
                 Abrir Whatsapp
             </button>
